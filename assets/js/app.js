d7204e732e1a3bfd2a2bb0161f254b110d4d8852
macamarambio <marambio.macarena@gmail.com>
2018-01-05 08:21:10 -0300

    solucionando conflictos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,73 +1,73 @@
 $(document).ready(()=> {
-	$('.language').hide();
-	$('.login').hide();
-	$('.principal').hide();
-	$('.profile').hide();
+  $('.language').hide();
+  $('.login').hide();
+  $('.principal').hide();
+  $('.profile').hide();
 
-	/*Vista splash con duración de 2 a 5 segundos*/
-	setTimeout(()=> {
-		$(".content").fadeOut(500);
-	},3000);
-	setTimeout(()=> {
-		$(".language").fadeIn(500);
-	});
+  /*Vista splash con duración de 2 a 5 segundos*/
+  setTimeout(()=> {
+    $(".content").fadeOut(500);
+  },3000);
+  setTimeout(()=> {
+    $(".language").fadeIn(500);
+  });
 
-	/*login*/
-	$('#btn1').click(()=>{
-		$('.language').hide();
-		$('.login').show();
-		$('.principal').hide();
-	});
-	$('#btnback').click(()=>{
-		$('.language').show();
-		$('.login').hide();
-	});
+  /*login*/
+  $('#btn1').click(()=>{
+    $('.language').hide();
+    $('.login').show();
+    $('.principal').hide();
+  });
+  $('#btnback').click(()=>{
+    $('.language').show();
+    $('.login').hide();
+  });
 
 /*Seccion Principal*/
-	$('#iniciar').click(()=>{
-		$('.language').hide();
-		$('.login').hide();
-		$('.principal').show();
-	});
+  $('#iniciar').click(()=>{
+    $('.language').hide();
+    $('.login').hide();
+    $('.principal').show();
+  });
 });
 
 /* PERFIL */
-	$('#perfil').click(()=>{
-		$('.language').hide();
-		$('.login').hide();
-		$('.principal').hide();
-		$('.profile').show();
-	})
+  $('#perfil').click(()=>{
+    $('.language').hide();
+    $('.login').hide();
+    $('.principal').hide();
+    $('.profile').show();
+  })
 
 /*Filtrar Restaurantes*/
 
 function filter(id) {
-	var filt = $('#selector').val();
-	$('#comida').val('');
-	if(id == 'comida'){
-		$('#comida').show();
-		$('#mexico').hide();
-		$('#japon').hide();
-		$('#thai').hide();
-	}
-	if(id == 'mexico'){
-		$('#comida').hide();
-		$('#mexico').show();
-		$('#japon').hide();
-		$('#thai').hide();
-	}
-	if(id == 'japon'){
-		$('#comida').hide();
-		$('#mexico').hide();
-		$('#japon').show();
-		$('#thai').hide();
-	}
-	if(id == 'thai'){
-		$('#comida').hide();
-		$('#mexico').hide();
-		$('#japon').hide();
-		$('#thai').show();
-	}
+  var filt = $('#selector').val();
+  $('#comida').val('');
+  if(id == 'comida'){
+    $('#comida').show();
+    $('#mexico').hide();
+    $('#japon').hide();
+    $('#thai').hide();
+  }
+  if(id == 'mexico'){
+    $('#comida').hide();
+    $('#mexico').show();
+    $('#japon').hide();
+    $('#thai').hide();
+  }
+  if(id == 'japon'){
+    $('#comida').hide();
+    $('#mexico').hide();
+    $('#japon').show();
+    $('#thai').hide();
+  }
+  if(id == 'thai'){
+    $('#comida').hide();
+    $('#mexico').hide();
+    $('#japon').hide();
+    $('#thai').show();
+  }
 }
 
 
@@ -76,70 +76,70 @@ function filter(id) {
 
 /* Intento para ocultar vista splash desde jQuery
 
-	$(document).ready(function() {
-	function splashMobile($, viewport){
+  $(document).ready(function() {
+  function splashMobile($, viewport){
 
-		if( viewport.is('<=sm') ) {
-			$('.language').hide();
-			$('.login').hide();
-			$('.principal').hide();
-		
-			setTimeout(()=> {
-				$(".content").fadeOut(500);
-			},3000);
-			setTimeout(()=> {
-				$(".language").fadeIn(500);
-			});
-		}if( viewport.is('>sm') ) {
-			$('.login').show();
-			$('.principal').hide();
-		}
-		}
+    if( viewport.is('<=sm') ) {
+      $('.language').hide();
+      $('.login').hide();
+      $('.principal').hide();
+    
+      setTimeout(()=> {
+        $(".content").fadeOut(500);
+      },3000);
+      setTimeout(()=> {
+        $(".language").fadeIn(500);
+      });
+    }if( viewport.is('>sm') ) {
+      $('.login').show();
+      $('.principal').hide();
+    }
+    }
 
-			$('#btn1').click(()=>{
-				$('.language').hide();
-				$('.login').show();
-				$('.principal').hide();
-			});
-			$('#btnback').click(()=>{
-				$('.language').show();
-				$('.login').hide();
-			});
+      $('#btn1').click(()=>{
+        $('.language').hide();
+        $('.login').show();
+        $('.principal').hide();
+      });
+      $('#btnback').click(()=>{
+        $('.language').show();
+        $('.login').hide();
+      });
 
-			$('#iniciar').click(()=>{
-				$('.language').hide();
-				$('.login').hide();
-				$('.principal').show();
-			});
+      $('#iniciar').click(()=>{
+        $('.language').hide();
+        $('.login').hide();
+        $('.principal').show();
+      });
 });
 
 function filter(id) {
-	var filt = $('#selector').val();
-	$('#comida').val('');
-	if(id == 'comida'){
-		$('#comida').show();
-		$('#mexico').hide();
-		$('#japon').hide();
-		$('#thai').hide();
-	}
-	if(id == 'mexico'){
-		$('#comida').hide();
-		$('#mexico').show();
-		$('#japon').hide();
-		$('#thai').hide();
-	}
-	if(id == 'japon'){
-		$('#comida').hide();
-		$('#mexico').hide();
-		$('#japon').show();
-		$('#thai').hide();
-	}
-	if(id == 'thai'){
-		$('#comida').hide();
-		$('#mexico').hide();
-		$('#japon').hide();
-		$('#thai').show();
-	}
+  var filt = $('#selector').val();
+  $('#comida').val('');
+  if(id == 'comida'){
+    $('#comida').show();
+    $('#mexico').hide();
+    $('#japon').hide();
+    $('#thai').hide();
+  }
+  if(id == 'mexico'){
+    $('#comida').hide();
+    $('#mexico').show();
+    $('#japon').hide();
+    $('#thai').hide();
+  }
+  if(id == 'japon'){
+    $('#comida').hide();
+    $('#mexico').hide();
+    $('#japon').show();
+    $('#thai').hide();
+  }
+  if(id == 'thai'){
+    $('#comida').hide();
+    $('#mexico').hide();
+    $('#japon').hide();
+    $('#thai').show();
+  }
 }
 
 
